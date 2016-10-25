a6512735d9a6894946226ab1931afb3743eb37ad
ckshei <cks856@gmail.com>
2016-10-25 19:27:34 -0400

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -3,23 +3,51 @@ const React = require('react');
 class LoginForm extends React.Component {
   constructor() {
     super();
+    
+    this.formSubmit = this.formSubmit.bind(this)
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+  
+  formSubmit(e) {
+      e.preventDefault()
+      const username = this.state.username
+      const password = this.state.password
+      if (!username || !password) {
+        return
+      } else {
+        this.props.onSubmit({
+          username,
+          password
+      })
+}
+}
+  
+  handlePasswordChange(e) {
+    this.setState({username: e.target.value});
+  }
 
-    this.state = {};
+  handleUsernameChange(e) {
+    this.setState({password: e.target.value});
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.formSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" onChange={this.handleUsernameChange}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" onChange={this.handlePasswordChange}/>
           </label>
         </div>
         <div>
@@ -31,3 +59,4 @@ class LoginForm extends React.Component {
 }
 
 module.exports = LoginForm;
+
