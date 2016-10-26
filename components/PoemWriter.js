12a13d3406341384e8263278b61fe487d80e2987
Raycent <raycent.tan@flatironschool.com>
2016-10-26 11:17:45 -0400

    Done.

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -3,15 +3,35 @@ const React = require('react');
 class PoemWriter extends React.Component {
   constructor() {
     super();
+    this.state = {
+      poem: "",
+      isValid: false
+    };
 
-    this.state = {};
+    this.handleEvent = this.handleEvent.bind(this)
+    this.isValid = this.isValid.bind(this)
+  }
+
+  handleEvent(ev) {
+    let poem = ev.target.value
+
+    if (poem) {
+      this.setState({
+        poem: poem,
+        isValid: this.isValid(poem)
+      });
+    }
+  }
+
+  isValid(poem) {
+    return poem.match(/[^\r\n]+/g).map(l => l.match(/\S+/g)).filter(l => l).map(l => l.length).join() == "5,3,5"
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" onChange={this.handleEvent}/>
+        {!this.state.isValid ? <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div> : null}
       </div>
     );
   }
