2153643b9cd9fba00a9b2ea6748c798789550e51
satub <satu.barnhill@gmail.com>
2016-11-07 10:53:13 -0500

    All tests pass, but poem validity checker doesn't work yet

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,22 +4,53 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+
+    this.state = {
+      pw: '',
+      un: ''
+    };
   }
+
+  handlePassword(event){
+    this.setState({
+      pw: event.target.value
+    });
+  }
+
+  handleUsername(event){
+    this.setState({
+      un: event.target.value
+    });
+  }
+
+  handleSubmission(event){
+    event.preventDefault();
+    let submissionParams = {password: this.state.pw, username: this.state.un};
+
+    if (!submissionParams.password || !submissionParams.username){
+      console.log("No, dear.")
+    } else {
+      this.props.onSubmit(submissionParams);
+    }
+  }
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmission}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.un} onChange={this.handleUsername}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.pw} onChange={this.handlePassword}/>
           </label>
         </div>
         <div>
--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -4,14 +4,41 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.handlePoem = this.handlePoem.bind(this);
+    this.validPoem = this.validPoem.bind(this);
+    this.state = {
+      poem: ''
+    };
+  }
+
+  handlePoem(event){
+    this.setState({
+      poem: event.target.value
+    })
+  }
+
+  validPoem(poem){
+    let validity = false;
+    let lines = poem.split("\n");
+    if (lines.length === 3){
+      validity = true;
+    }
+    ////Currently only checks for this yet all the poem tests pass, hmmpff.
+    // The poem has three lines.
+
+    ////Still need to test for these
+    
+    // The first has five words.
+    // Then three words.
+    // The third has five words.
+    return validity;
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value = {this.state.poem} onChange={this.handlePoem}/>
+        {this.validPoem(this.state.poem) ? '' : <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>}
       </div>
     );
   }
